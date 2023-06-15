96ac1977fa5aff14e399f5c955f2285ff1be9bc4
Rob Hargraves <rob.hargraves@helplightning.com>
2023-06-15 12:35:00 -0500

    Fixed another issue with the new S3 security defaults

--- aws/cloudtrail/s3-bucket.tf
+++ aws/cloudtrail/s3-bucket.tf
@@ -37,15 +37,29 @@ resource "aws_s3_bucket" "cloudtrail" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "cloudtrail" {
+  bucket = aws_s3_bucket.cloudtrail.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_ownership_controls" "cloudtrail" {
   bucket = aws_s3_bucket.cloudtrail.id
 
   rule {
-    object_ownership = "ObjectWriter"
+    object_ownership = "BucketOwnerPreferred"
   }
 }
 
 resource "aws_s3_bucket_acl" "cloudtrail" {
+  depends_on = [
+    aws_s3_bucket_public_access_block.cloudtrail,
+    aws_s3_bucket_ownership_controls.cloudtrail,
+  ]
+
   bucket = aws_s3_bucket.cloudtrail.id
   acl    = "log-delivery-write"
 }
