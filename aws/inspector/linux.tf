4dc76ad0cc8166fb9ed245b1b2904a4185ac5bb1
Rob Hargraves <rob.hargraves@helplightning.com>
2023-06-29 11:11:41 -0500

    aws/inspector: Use the appropriate inspector rules per region

--- aws/inspector/linux.tf
+++ aws/inspector/linux.tf
@@ -14,9 +14,9 @@ resource "aws_inspector_assessment_template" "linux" {
   duration = 3600
   name     = "${var.account_name}-linux"
   rules_package_arns = [
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-R01qwB5Q",
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gBONHN9h",
-    "arn:aws:inspector:us-east-1:316112463485:rulespackage/0-gEjTy7T7",
+    local.regional_rules_package_arns[data.aws_region.current.name]["security_best_practices"],
+    local.regional_rules_package_arns[data.aws_region.current.name]["runtime_behavior_analysis"],
+    local.regional_rules_package_arns[data.aws_region.current.name]["common_vulnerabilities_and_exposures"],
   ]
   target_arn = aws_inspector_assessment_target.linux.arn
 }
